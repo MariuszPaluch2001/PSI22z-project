5487b06390fcb85a45da607bdd67677d52d8ed5a
Filip Leszek <filip.leszek@icloud.com>
2023-01-02 16:07:00 +0100

    added types

--- projekt/packets.py
+++ projekt/packets.py
@@ -2,10 +2,12 @@ from datetime import datetime
 import struct
 import time
 
+from packets_exceptions import *
+
 
 class Packet:
 
-    def __init__(self, session_id, packet_number) -> None:
+    def __init__(self, session_id: int, packet_number: int) -> None:
         self.packet_type = 0
         self.session_id = session_id
         self.packet_number = packet_number
@@ -45,7 +47,7 @@ class Packet:
 
 class ControlPacket(Packet):
 
-    def __init__(self, session_id, packet_number, control_type) -> None:
+    def __init__(self, session_id: int, packet_number: int, control_type: str) -> None:
         super().__init__(session_id, packet_number)
 
         if (control_type not in ['o', 's', 'c']):
@@ -55,14 +57,14 @@ class ControlPacket(Packet):
 
 class SessionControlPacket(ControlPacket):
 
-    def __init__(self, session_id, packet_number, control_type) -> None:
+    def __init__(self, session_id: int, packet_number: int, control_type: str) -> None:
         super().__init__(session_id, packet_number, control_type)
         self.packet_type = 1
 
 
 class StreamControlPacket(ControlPacket):
 
-    def __init__(self, session_id, packet_number, control_type, stream_id) -> None:
+    def __init__(self, session_id: int, packet_number: int, control_type, stream_id: int) -> None:
         super().__init__(session_id, packet_number, control_type)
         self.packet_type = 2
         self.stream_id = stream_id
@@ -70,7 +72,7 @@ class StreamControlPacket(ControlPacket):
 
 class RetransmissionRequestPacket(Packet):
 
-    def __init__(self, session_id, packet_number, stream_id, requested_packet_number) -> None:
+    def __init__(self, session_id: int, packet_number: int, stream_id: int, requested_packet_number: int) -> None:
         super().__init__(session_id, packet_number)
         self.packet_type = 3
         self.stream_id = stream_id
@@ -79,24 +81,32 @@ class RetransmissionRequestPacket(Packet):
 
 class ConfirmationPacket(Packet):
 
-    def __init__(self, session_id, packet_number, stream_id, data) -> None:
+    def __init__(self, session_id: int, packet_number: int, stream_id: int, data: str) -> None:
         super().__init__(session_id, packet_number)
         self.packet_type = 4
         self.stream_id = stream_id
+
+        if (len(data) > 100):
+            raise TooLongDataError()
+
         self.data = data
 
 
 class DataPacket(Packet):
 
-    def __init__(self, session_id, stream_id, packet_number, data) -> None:
+    def __init__(self, session_id: int, stream_id: int, packet_number: int, data: str) -> None:
         super().__init__(session_id, packet_number)
         self.packet_type = 5
         self.stream_id = stream_id
         self.timestamp = int(time.mktime(datetime.now().timetuple()))
+
+        if (len(data) > 100):
+            raise TooLongDataError()
+
         self.data = data
 
 
 class ErrorPacket(Packet):
 
-    def __init__(self, session_id, packet_number) -> None:
+    def __init__(self, session_id: int, packet_number: int) -> None:
         super().__init__(session_id, packet_number)
