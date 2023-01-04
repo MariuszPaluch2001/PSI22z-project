276b6b2b41b9b6c32353582d718212485680e63d
Kacper Kozerski <01158764@pw.edu.pl>
2023-01-04 13:08:04 +0100

    Improved demo

--- projekt/server_main.py
+++ projekt/server_main.py
@@ -2,24 +2,41 @@ from session import *
 from datetime import datetime, timedelta
 import threading
 import time
+
+CLIENT_ADDR = '127.0.0.1'
+CLIENT_PORT = 8004
+SERVER_ADDR = '127.0.0.1'
+SERVER_PORT = 8005
+CLIENT_WORK = 120
+
 def stream_to_file(stream: ClientStream, filename: str, working_time: int):
     time = datetime.now()
+    #strumień klienta pracuje dopóki nie minie założony czas lub nie zostanie zamknięty
     def work():
         return not stream.is_closed() and (datetime.now() - time) < timedelta(seconds=working_time)
+    f_text = open(filename, 'wb')
+    f_stamped = open(filename + '.stamped', 'wb')
+    while work():
+        #klient oczekuje maksymalnie 1 sekundę na nową, poprawną wiadomość
+        if packet := stream.get_message(1):
+            #pisanie do pliku znakowanego i do pliku tekstowego
+            f_text.write(packet.data)
+            f_stamped.write(str(packet.timestamp).encode('ascii'))
+            f_stamped.write(packet.data)
 
-    with open(filename, "wb") as f:
-        while work():
-            packet = stream.get_message(5)
-            if packet:
-                f.write(packet.data)
+    f_text.close()
+    f_stamped.close()
 
 def client_dispatch(session: ClientSession):
     time = datetime.now()
+    #klient pracuje przez 200 sekund lub do zamknięcia
     def work():
-        return not session.is_closed and (datetime.now() - time) < timedelta(seconds=120)
+        return not session.is_closed and (datetime.now() - time) < timedelta(seconds=CLIENT_WORK)
     
     while work():
-        session.receive_packets(3,3)
+        #klient odbiera do 5 paczek z timeoutem = 1s
+        session.receive_packets(5,1)
+        #klient rozsyła paczki kontrolne, niepotwierdzone paczki i paczki wzięte od strumieni
         session.send_packets()
     session.close()
     
@@ -27,52 +44,57 @@ def client_dispatch(session: ClientSession):
 def file_to_stream(stream: ServerStream, filename: str):
     with open(filename, "rb") as f:
         full_data = f.readlines()
+        #praca aż do zamknięcia lub wykonania zadania
         def work():
             return not stream.is_closed() and len(full_data) > 0
         
         while work():
+            #strumień serwera procesuje paczki kontrolne
             stream.process_control_packets()
+            #strumień serwera wysyła porcję danych
             data = full_data.pop(0)
             stream.put_data(data)
+            #spanie do demonstracji działania znakowania czasowego
+            time.sleep(1)
 
 def server_dispatch(session: ServerSession):
     def work():
         return not session.is_closed
     while work():
-        session.receive_packets(3,3)
+        #serwer oczekuje na maks. 5 paczek z timeoutem=1s
+        session.receive_packets(5,1)
+        #gdy serwer dostanie polecenie otwarcia strumienia, to tworzy go
+        #i przydziela mu pracę w nowym wątku
         for new_stream in session.get_new_streams():
-            filename = 'file' + str(new_stream.stream_id) + '.txt'
+            filename = 'file' + str(new_stream.stream_id)
             threading.Thread(target=file_to_stream, args=(
                 new_stream,
                 filename,
             ), daemon=True).start()
+        #serwer wysyła pakiety odebrane od strumieni oraz potwierdzenia paczek
         session.send_packets()
 
-CLIENT_ADDR = '127.0.0.1'
-CLIENT_PORT = 8004
-SERVER_ADDR = '127.0.0.1'
-SERVER_PORT = 8005
+
 
 
 def client_main():
+    #klienta czeka na ustawienie serwera
     time.sleep(5)
     client = ClientSession()
     client.connect(CLIENT_ADDR, CLIENT_PORT, SERVER_ADDR, SERVER_PORT)
-    stream1 = client.open_new_stream()
-    stream2 = client.open_new_stream()
-    stream3 = client.open_new_stream()
-    thread1 = threading.Thread(target=stream_to_file, args=(stream1, 'worker1.txt',100))
-    thread2 = threading.Thread(target=stream_to_file, args=(stream2, 'worker2.txt',100))
-    thread3 = threading.Thread(target=stream_to_file, args=(stream3, 'worker3.txt',70))
-    
-    thread1.start()
-    thread2.start()
-    thread3.start()
+    threads = [
+        threading.Thread(target=stream_to_file, args=(
+            client.open_new_stream(),
+            'worker' + str(i) + '.txt', 
+            random.randint(CLIENT_WORK-40, CLIENT_WORK+40)
+        ))
+        for i in range(1,9)
+    ]
+    for thread in threads:
+        thread.start()
     client_dispatch(client)
-    thread1.join()
-    thread2.join()
-    thread3.join()
-
+    for thread in threads:
+        thread.join()
 
 
 def server_main():
