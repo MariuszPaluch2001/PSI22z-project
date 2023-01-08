a402324695f0ea47abba860fc4958906c2d3de14
Dominik Łopatecki <dominik.lopatecki.stud@pw.edu.pl>
2023-01-08 18:31:38 +0100

    Added try except block

--- projekt/Parser.py
+++ projekt/Parser.py
@@ -15,31 +15,34 @@ class Parser:
                         5: "@hiiii"}
 
     def parse_packet(self, input_data) -> packets.Packet:
-        packet_type = input_data[0]
+        try:
+            packet_type = input_data[0]
 
-        if packet_type not in self.formats.keys():
-            raise Exception("Incorrect type of packet")
+            if packet_type not in self.formats.keys():
+                raise Exception("Incorrect type of packet")
 
-        format = self.formats[packet_type]
-        packet_fields = struct.unpack_from(format, input_data)
+            format = self.formats[packet_type]
+            packet_fields = struct.unpack_from(format, input_data)
 
-        if packet_type == 0:
-            return packets.ErrorPacket(*packet_fields[1:])
+            if packet_type == 0:
+                return packets.ErrorPacket(*packet_fields[1:])
 
-        if packet_type == 1:
-            control_type = packet_fields[3].decode('ascii')
-            return packets.SessionControlPacket(*packet_fields[1:3], control_type)
+            if packet_type == 1:
+                control_type = packet_fields[3].decode('ascii')
+                return packets.SessionControlPacket(*packet_fields[1:3], control_type)
 
-        if packet_type == 2:
-            control_type = packet_fields[3].decode('ascii')
-            return packets.StreamControlPacket(*packet_fields[1:3], control_type, packet_fields[4])
+            if packet_type == 2:
+                control_type = packet_fields[3].decode('ascii')
+                return packets.StreamControlPacket(*packet_fields[1:3], control_type, packet_fields[4])
 
-        if packet_type == 3:
-            return packets.RetransmissionRequestPacket(*packet_fields[1:])
+            if packet_type == 3:
+                return packets.RetransmissionRequestPacket(*packet_fields[1:])
 
-        if packet_type == 4:
-            return packets.ConfirmationPacket(*packet_fields[1:])
+            if packet_type == 4:
+                return packets.ConfirmationPacket(*packet_fields[1:])
 
-        if packet_type == 5:
-            data = input_data[20:]
-            return packets.DataPacket(*packet_fields[1:4], data, packet_fields[4])
+            if packet_type == 5:
+                data = input_data[20:]
+                return packets.DataPacket(*packet_fields[1:4], data, packet_fields[4])
+        except Exception as e:
+            print(e)
